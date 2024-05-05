13b9fd3adf230a312c6ce38826861a77e26d929f
Cristian-hash <cpacori@unsa.edu.pe>
2024-05-05 11:05:32 -0500

    Edicion de la logica de MostrarDatos.java

--- app/src/main/java/com/example/myformulario/MostrarDatos.java
+++ app/src/main/java/com/example/myformulario/MostrarDatos.java
@@ -1,6 +1,10 @@
 package com.example.myformulario;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +13,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MostrarDatos extends AppCompatActivity {
+    TextView tvnombre,tvapellidos,tvedad,tvcorreo;
+    Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //campos que tenemos
-
-
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mostrar_datos);
-        
+
+        tvnombre = (TextView)findViewById(R.id.tvNombre);
+        tvapellidos = (TextView)findViewById(R.id.tvApellidos);
+        tvedad = (TextView)findViewById(R.id.tvEdad);
+        tvcorreo = (TextView)findViewById(R.id.tvCorreo);
+        btnOk = (Button)findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MostrarDatos.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         mostrarDato();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -31,6 +46,15 @@ public class MostrarDatos extends AppCompatActivity {
     }
 
     private void mostrarDato() {
+        Bundle datos = this.getIntent().getExtras();
+        String nombre = datos.getString("name");
+        String apellidos = datos.getString("ape");
+        String edad = datos.getString("age");
+        String correo = datos.getString("email");
 
+        tvnombre.setText(nombre);
+        tvapellidos.setText(apellidos);
+        tvedad.setText(edad);
+        tvcorreo.setText(correo);
     }
 }
